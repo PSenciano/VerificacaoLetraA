6eeb605ee050a8ced5ddd7855a596dd15c71f796
Pedro <psenciano@gmail.com>
2024-10-15 13:53:23 -0300

    implementação do desafio

---
+++ VerificacaoLetraA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Informe uma string: ");
+        string entrada = Console.ReadLine();
+        int quantidade = ContarLetraA(entrada);
+
+        Console.WriteLine($"A letra 'a' (maiúscula ou minúscula) aparece {quantidade} vez(es) na string.");
+    }
+
+    static int ContarLetraA(string str)
+    {
+        int quantidade = 0;
+        foreach (char c in str)
+        {
+            if (char.ToLower(c) == 'a')
+            {
+                quantidade++;
+            }
+        }
+        return quantidade;
+    }
+}
